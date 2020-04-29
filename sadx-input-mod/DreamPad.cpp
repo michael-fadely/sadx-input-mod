3957f32e8973836f441fdec8012f37ac8c8dca6a
PkR <pkr@pkr.pkr>
2020-04-29 11:30:35 +0900

    Use Buttons enum instead of 0x606

--- sadx-input-mod/DreamPad.cpp
+++ sadx-input-mod/DreamPad.cpp
@@ -260,7 +260,7 @@ void DreamPad::poll()
 		normalized_r_ /= 2.0f;
 	}
 
-	if (dc_pad.HeldButtons == 0x606 && dc_pad.ReleasedButtons == Buttons_Start && GameMode != 1 && GameMode != 8)
+	if (dc_pad.HeldButtons == (Buttons_A | Buttons_B | Buttons_X | Buttons_Y) && dc_pad.ReleasedButtons == Buttons_Start && GameMode != 1 && GameMode != 8)
 	{
 		if (input::debug) PrintDebug("Soft reset\n");
 		WriteData<1>((char*)0x3B0EAA0, 0x01u);
